5abe0b5ec554d110f3ff6c17c68341f1768e021e
AzharJacobs <azharj2003@gmail.com>
2024-07-03 11:46:13 +0200

    step1

---
+++ src/components/ResumeSection.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="container">
+    <div class="row pt-5">
+      <h2 class="text">Education</h2>
+    </div>
+    <div class="row pt-5">
+      <div class="col d-flex justify-content-center">
+        <div class="timeline">
+          <div v-for="(item, index) in education" :key="index" :class="['timeline-container', index % 2 === 0 ? 'left-container' : 'right-container']">
+            <img src="https://i.pinimg.com/originals/cd/65/15/cd6515604230d57b71983853c4156752.png" alt="">
+            <div class="text-box">
+              <h2>{{ item.institution }}</h2>
+              <small>{{ item.year }}</small>
+              <p>{{ item.qualification }}</p>
+              <span :class="[index % 2 === 0 ? 'left-container-arrow' : 'right-container-arrow']"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResumeSection',
+  props: {
+    education: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
+.timeline {
+  position: relative;
+  max-width: 1200px;
+  margin: 100px auto;
+  /* animation: movedown 1s linear forwards; */
+}
+
+.timeline::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 2px;
+  background: linear-gradient(235deg, #B8860B, #010615, #B8860B);
+  left: 50%;
+  margin-left: -1px;
+}
+
+.timeline-container {
+  padding: 10px 40px;
+  position: relative;
+  width: 50%;
+  box-sizing: border-box;
+  opacity: 0;
+  animation: movedown 1s linear forwards;
+  
+}
+
+@keyframes movedown {
+  0% {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+}
+
+.timeline-container.left-container {
+  left: -50%;
+}
+
+.timeline-container.right-container {
+  left: 50%;
+}
+
+.timeline-container img {
+    position: absolute;
+    width: 40px;
+    border-radius: 50%;
+    right: -20px;
+    top: 32px;
+    z-index: 10;
+  }
+
+  .right-container img {
+    left: -20px;
+  }
+
+  .left-container img {
+    left: 460px;
+  }
+
+.text-box {
+  padding: 20px 30px;
+  background: #000;
+  position: relative;
+  border-radius: 6px;
+  font-size: 15px;
+  color: #fff;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  font-family: 'Poppins', sans-serif;
+  border: 3px solid;
+  border-image-slice: 1;
+  border-width: 3px;
+  border-image-source:  linear-gradient(235deg, #B8860B, #010615, #B8860B);
+  width: 400px;
+}
+
+.text-box h2 {
+  font-weight: 600;
+  font-size: 22px;
+}
+
+.text-box small {
+  display: block;
+  margin-bottom: 15px;
+}
+
+.left-container-arrow {
+  height: 0;
+  width: 0;
+  position: absolute;
+  top: 28px;
+  z-index: 1;
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  border-left: 15px solid #B8860B;
+  right: -15px;
+}
+
+.right-container-arrow {
+  height: 0;
+  width: 0;
+  position: absolute;
+  top: 28px;
+  z-index: 1;
+  border-top: 15px solid transparent;
+  border-bottom: 15px solid transparent;
+  border-right: 15px solid #B8860B;
+  left: -15px;
+}
+
+@media screen and (max-width: 600px) {
+  .timeline::before {
+    left: 31px;
+  }
+
+  .timeline-container {
+    width: 100%;
+    padding-left: 70px;
+    padding-right: 25px;
+  }
+
+  .text-box {
+    font-size: 13px;
+  }
+
+  .right-container {
+    left: 0%;
+  }
+
+  .timeline-container img {
+    left: 10px;
+  }
+
+  .left-container-arrow,
+  .right-container-arrow {
+    border-right: 15px solid #000;
+    border-left: 0;
+    left: -15px;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <section class="row" id="home">
+      <HomeSection/>
+    </section>
+    <section class="row" id="about"></section>
+    <section class="row" id="resume"></section>
+    <section class="row" id="projects"></section>
+    <section class="row" id="testimonials"></section>
+    <section class="row" id="contact"></section>
+  </div>
+</template>
+
+<script>
+// import HomeSection from '@/components/HomeSection.vue'
+
+export default {
+  // components: {
+  //   HomeSection
+  // }
+};
+</script>
