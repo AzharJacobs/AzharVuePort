cfaed8701d354785c9135d04e100b01d751fa3fd
AzharJacobs <azharj2003@gmail.com>
2024-07-04 09:57:09 +0200

    Project&Skills

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,11 @@
       <section id="resume">
         <resume-section :education="education"/>
       </section>
+      <section id="Skills">
+        <SkillsSection :skills="skills"/>
+      </section>
       <section id="projects">
+        <project-section :projects="projects"/>
       </section>
       <section id="testimonials"></section>
       <section id="contact"></section>
@@ -40,6 +44,9 @@ import Footer from '@/components/Footer.vue'
 import HomeSection from '@/components/HomeSection.vue'
 import AboutSection from '@/components/AboutSection.vue'
 import ResumeSection from '@/components/ResumeSection.vue'
+import ProjectSection from '@/components/ProjectSection.vue'
+import SkillsSection from '@/components/SkillsSection.vue'
+
 
 export default {
   computed: { 
@@ -51,19 +58,30 @@ export default {
     },
     education () {
       return this.$store.state.education
+    },
+    projects () {
+      return this.$store.state.projects
+    },
+    skills () {
+      return this.$store.state.skills
     }
+
   },
   components: {
     NavBar,
     Footer,
     HomeSection,
     AboutSection,
-    ResumeSection
+    ResumeSection,
+    ProjectSection,
+    SkillsSection
   },
   mounted() {
     this.$store.dispatch('getAboutMe'),
     this.$store.dispatch('getHobbies'),
-    this.$store.dispatch('getEducation')
+    this.$store.dispatch('getEducation'),
+    this.$store.dispatch('getprojects'),
+    this.$store.dispatch('getSkills')
   }
 }
 </script>
